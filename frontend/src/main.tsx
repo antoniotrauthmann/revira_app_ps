82afc5bbd6ae170218a2bc2e4578a1d6860b2742
antoniotrauthmann <antonio.trauthmanpmw@gmail.com>
2026-09-13 19:10:39 -0300

    Revert "tela do chat"
    This reverts commit cb376004a9ed3c40adee930e126f8e00c34a47dd.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Perfil from './Profile.tsx';
 import './index.css'
 import App from './App.tsx'
 
@@ -9,7 +8,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-
-export default function App() {
-  return <Perfil />;
-}
